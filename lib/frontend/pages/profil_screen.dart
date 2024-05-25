9cecee4e0d03282517ec112b261376746f913122
Gad Ntenta <gadntenta243@gmail.com>
2024-05-25 09:00:47 +0100

    actualisation de la page profile, affiche le nom et prenom du user

--- lib/frontend/pages/profil_screen.dart
+++ lib/frontend/pages/profil_screen.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:toubibplus/backend/firebase/authentification/deconnexion_service.dart';
 import 'package:toubibplus/frontend/messages/toast.dart';
-
+import 'package:toubibplus/frontend/pages/user_connecter.dart';
 import '../blocks/profil_menu/profil_menu.dart';
 import '../composants/image_icone/imageicone.dart';
 import 'Home.dart';
@@ -13,7 +14,7 @@ import 'message_screen.dart';
 import 'rdv_screen.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({Key? key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -21,6 +22,86 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   int _selectedIndex = 4;
+  String? userName;
+  String? userSurname;
+  String? userProfileUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserProfile();
+  }
+
+  Future<void> _loadUserProfile() async {
+    if (globalUserID != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(globalUserID).get();
+      if (userDoc.exists) {
+        Map<String, dynamic>? userData = userDoc.data() as Map<String, dynamic>?;
+
+        setState(() {
+          userName = userData?['nom'] as String?;
+          userSurname = userData?['prenom'] as String?;
+          userProfileUrl = userData?['profile'] as String?;
+        });
+
+        _checkAndShowNameInputDialog();
+      }
+    }
+  }
+
+
+  void _checkAndShowNameInputDialog() {
+    if (userName == null || userSurname == null || userName!.isEmpty || userSurname!.isEmpty) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            final TextEditingController nameController = TextEditingController();
+            final TextEditingController surnameController = TextEditingController();
+
+            return AlertDialog(
+              title: Text('Veuillez saisir votre nom et prénom'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(labelText: 'Nom'),
+                  ),
+                  TextField(
+                    controller: surnameController,
+                    decoration: InputDecoration(labelText: 'Prénom'),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () async {
+                    if (nameController.text.isNotEmpty && surnameController.text.isNotEmpty) {
+                      await FirebaseFirestore.instance.collection('users').doc(globalUserID).update({
+                        'nom': nameController.text,
+                        'prenom': surnameController.text,
+                      });
+
+                      setState(() {
+                        userName = nameController.text;
+                        userSurname = surnameController.text;
+                      });
+
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: Text('Enregistrer'),
+                ),
+              ],
+            );
+          },
+        );
+      });
+    }
+  }
+
 
   void _onItemTapped(int index) {
     setState(() {
@@ -81,8 +162,10 @@ class _ProfilePageState extends State<ProfilePage> {
             clipBehavior: Clip.none,
             fit: StackFit.expand,
             children: [
-              const CircleAvatar(
-                backgroundImage: AssetImage('assets/squelettons/avatar-man-b_w.png'),
+              CircleAvatar(
+                backgroundImage: userProfileUrl != null
+                    ? NetworkImage(userProfileUrl!)
+                    : const AssetImage('assets/squelettons/avatar-man-b_w.png') as ImageProvider,
               ),
               Positioned(
                 right: -12,
@@ -91,27 +174,27 @@ class _ProfilePageState extends State<ProfilePage> {
                   height: 46,
                   width: 46,
                   child: FloatingActionButton(
-                  // Utilisation de BoxDecoration au lieu de la couleur directe
-                  shape: const CircleBorder(
-                    side: BorderSide(color: Colors.white),
+                    // Utilisation de BoxDecoration au lieu de la couleur directe
+                    shape: const CircleBorder(
+                      side: BorderSide(color: Colors.white),
+                    ),
+                    onPressed: () {},
+                    child: SvgPicture.asset(
+                      "assets/Icons/Camera.svg",
+                      width: 22.0,
+                      height: 22.0,
+                      color: const Color(0xFF66DED4),
+                    ),
                   ),
-                  onPressed: () {},
-                  child: SvgPicture.asset(
-                    "assets/Icons/Camera.svg",
-                    width: 22.0,
-                    height: 22.0,
-                    color: const Color(0xFF66DED4),
-                  ),
-                ),
                 ),
               ),
             ],
           ),
         ),
-        const Text(
-          'Amelia Renata',
+        Text(
+          userName != null && userSurname != null ? '$userName $userSurname' : '',
           textAlign: TextAlign.center,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 16,
           ),
@@ -163,45 +246,46 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget _buildProfileMenu() {
-    return  Column(
-        children: [
-          ProfilMenu(
-            icon: 'assets/Icons/coeur.svg',
-            text: 'Ma sélection',
-            press: () {},
-          ),
-          ProfilMenu(
-            icon: 'assets/Icons/Document.svg',
-            text: 'Rendez-vous',
-            press: () {},
-          ),
-          ProfilMenu(
-            icon: 'assets/Icons/Wallet.svg',
-            text: 'Fiche de santé',
-            press: () {},
-          ),
-          ProfilMenu(
-            icon: 'assets/Icons/Message.svg',
-            text: 'FAQs',
-            press: () {},
-          ),
-          ProfilMenu(
-            icon: 'assets/Icons/Danger_Circle.svg',
-            text: 'Déconnexion',
-            press: () {
-              signOutAndNavigate(context, showToast);
-              // Navigation vers l'écran de connexion
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const Connexion(),
-                ),
-              );
-            },
-          ),
-        ],
+    return Column(
+      children: [
+        ProfilMenu(
+          icon: 'assets/Icons/coeur.svg',
+          text: 'Ma sélection',
+          press: () {},
+        ),
+        ProfilMenu(
+          icon: 'assets/Icons/Document.svg',
+          text: 'Rendez-vous',
+          press: () {},
+        ),
+        ProfilMenu(
+          icon: 'assets/Icons/Wallet.svg',
+          text: 'Fiche de santé',
+          press: () {},
+        ),
+        ProfilMenu(
+          icon: 'assets/Icons/Message.svg',
+          text: 'FAQs',
+          press: () {},
+        ),
+        ProfilMenu(
+          icon: 'assets/Icons/Danger_Circle.svg',
+          text: 'Déconnexion',
+          press: () {
+            signOutAndNavigate(context, showToast);
+            // Navigation vers l'écran de connexion
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const Connexion(),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
+
   Widget ProfilMenu({
     required String icon,
     required String text,
@@ -245,23 +329,17 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-
   Widget _buildBottomNavigationBar() {
     return BottomNavigationBar(
       selectedItemColor:
       const Color(0xFF66DED4), // Couleur de l'élément sélectionné
       unselectedItemColor:
       Colors.grey.shade700, // Couleur des éléments non sélectionnés
-      type: BottomNavigationBarType
-          .fixed, // Type de la barre de navigation (fixe)
-      showSelectedLabels:
-      false, // Ne pas afficher les labels des éléments sélectionnés
-      showUnselectedLabels:
-      false, // Ne pas afficher les labels des éléments non sélectionnés
-      currentIndex:
-      _selectedIndex, // Index de l'élément actuellement sélectionné
-      onTap:
-      _onItemTapped, // Fonction appelée lorsque l'utilisateur sélectionne un élément
+      type: BottomNavigationBarType.fixed, // Type de la barre de navigation (fixe)
+      showSelectedLabels: false, // Ne pas afficher les labels des éléments sélectionnés
+      showUnselectedLabels: false, // Ne pas afficher les labels des éléments non sélectionnés
+      currentIndex: _selectedIndex, // Index de l'élément actuellement sélectionné
+      onTap: _onItemTapped, // Fonction appelée lorsque l'utilisateur sélectionne un élément
       items: [
         BottomNavigationBarItem(
           icon: ImageIcone1(
@@ -305,5 +383,73 @@ class _ProfilePageState extends State<ProfilePage> {
         ),
       ],
     );
+  }
+
+  Future<void> _showNameInputDialog() async {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController surnameController = TextEditingController();
+    bool isFormValid = false;
+
+    await showDialog<void>(
+      context: context,
+      barrierDismissible: false, // Le popup ne peut pas être fermé en cliquant en dehors
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: const Text('Veuillez saisir votre nom et prénom'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(hintText: 'Nom'),
+                      onChanged: (value) {
+                        setState(() {
+                          isFormValid = nameController.text.isNotEmpty && surnameController.text.isNotEmpty;
+                        });
+                      },
+                    ),
+                    TextField(
+                      controller: surnameController,
+                      decoration: const InputDecoration(hintText: 'Prénom'),
+                      onChanged: (value) {
+                        setState(() {
+                          isFormValid = nameController.text.isNotEmpty && surnameController.text.isNotEmpty;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Enregistrer'),
+                  onPressed: isFormValid
+                      ? () {
+                    _saveUserNameAndSurname(nameController.text, surnameController.text);
+                    Navigator.of(context).pop();
+                  }
+                      : null,
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<void> _saveUserNameAndSurname(String name, String surname) async {
+    if (globalUserID != null) {
+      await FirebaseFirestore.instance.collection('users').doc(globalUserID).update({
+        'nom': name,
+        'prenom': surname,
+      });
+      setState(() {
+        userName = name;
+        userSurname = surname;
+      });
+    }
   }
 }
